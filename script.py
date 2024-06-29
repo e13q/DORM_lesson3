41671fd5fa0a4a4db8ad3404a657f0617e906d2d
e13q <110967581+e13q@users.noreply.github.com>
2024-06-29 14:06:55 +0300

    1'st edit

--- script.py
+++ script.py
@@ -6,9 +6,6 @@ from datacenter.models import Chastisement
 from datacenter.models import Subject
 from datacenter.models import Commendation
 from datacenter.models import Lesson
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.exceptions import MultipleObjectsReturned
-from django.db.models import Q
 
 
 COMMENDATIONS = [
@@ -47,54 +44,46 @@ COMMENDATIONS = [
 
 def get_schoolkid(name_schoolkid):
     try:
-        if name_schoolkid and len(name_schoolkid) > 3:
-            schoolkid = Schoolkid.objects.get(
-                full_name__contains=name_schoolkid
-            )
-            return schoolkid
-        else:
-            print('Ошибка. Длинна имени должна быть больше 3 симв.')
-    except ObjectDoesNotExist:
+        schoolkid = Schoolkid.objects.get(
+            full_name__contains=name_schoolkid
+        )
+        return schoolkid
+    except Schoolkid.DoesNotExist:
         print("Ошибка. Ученик не найден")
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print("Ошибка. Найдено несколько учеников, вместо одного")
 
 
 def get_subject(name_subject, year_of_study):
     try:
-        if name_subject and len(name_subject) > 3:
-            subject = Subject.objects.get(
-                Q(title__contains=name_subject) &
-                Q(year_of_study=year_of_study)
-            )
-            return subject
-        else:
-            print('Ошибка. Длинна назв. предмета должна быть больше 3 симв.')
-    except ObjectDoesNotExist:
+        subject = Subject.objects.get(
+            title__contains=name_subject,
+            year_of_study=year_of_study
+        )
+        return subject
+    except Subject.DoesNotExist:
         print("Ошибка. Предмет не найден")
-    except MultipleObjectsReturned:
+    except Subject.MultipleObjectsReturned:
         print("Ошибка. Найдено несколько предметов, вместо одного")
 
 
 def get_commendation(date, schoolkid, subject, teacher):
-    try:
-        commendation = Commendation.objects.get(
-            Q(created=date) &
-            Q(schoolkid=schoolkid) &
-            Q(subject=subject) &
-            Q(teacher=teacher)
-        )
-        return commendation
-    except ObjectDoesNotExist:
+    commendation = Commendation.objects.filter(
+        created=date,
+        schoolkid=schoolkid,
+        subject=subject,
+        teacher=teacher
+    )
+    if commendation.exists():
+        return commendation.first()
+    else:
         return None
-    except MultipleObjectsReturned:
-        return []
 
 
 def fix_marks(name_schoolkid=''):
     schoolkid = get_schoolkid(name_schoolkid)
     if schoolkid:
-        marks = Mark.objects.filter(Q(schoolkid=schoolkid) & Q(points__lt=4))
+        marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
         count_of_updated = marks.update(points=5)
         if count_of_updated:
             print(f'{count_of_updated} плохих оценок заменено')
@@ -116,10 +105,9 @@ def create_commendation(name_schoolkid='', name_subject=''):
     subject = schoolkid and get_subject(name_subject, schoolkid.year_of_study)
     if subject:
         lessons = Lesson.objects.filter(
-            Q(year_of_study__contains=schoolkid.year_of_study) &
-            Q(subject=subject) &
-            Q(group_letter='А')
-
+            year_of_study__contains=schoolkid.year_of_study,
+            subject=subject,
+            group_letter=schoolkid.group_letter
         )
         if lessons[0]:
             lesson_newest = lessons.order_by('-date').first()
